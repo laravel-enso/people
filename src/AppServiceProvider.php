27f0edb730ebde9a790efdebd2dda74b62ef4138
mishu <mishu@xtelecom.ro>
2018-10-09 12:20:51 +0300

    removes deprecated seeder

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -31,14 +31,6 @@ class AppServiceProvider extends ServiceProvider
         ], 'enso-assets');
 
         $this->publishes([
-            __DIR__.'/database/seeds' => database_path('seeds'),
-        ], 'people-seeder');
-
-        $this->publishes([
-            __DIR__.'/database/seeds' => database_path('seeds'),
-        ], 'enso-seeders');
-
-        $this->publishes([
             __DIR__.'/database/factories' => database_path('factories'),
         ], 'people-factory');
 
