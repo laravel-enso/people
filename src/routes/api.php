8ec9023819c97efc6b20f854d3832c4a19b5d720
mishu <mishu@xtelecom.ro>
2018-10-21 12:08:30 +0300

    fixes routes file code style

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,17 +6,17 @@ Route::namespace('LaravelEnso\People\app\Http\Controllers')
     ->as('administration.')
     ->group(function () {
         Route::prefix('people')->as('people.')
-        ->group(function () {
-            Route::get('options', 'PersonSelectController@options')
-                ->name('options');
+            ->group(function () {
+                Route::get('initTable', 'PersonTableController@init')
+                    ->name('initTable');
+                Route::get('getTableData', 'PersonTableController@data')
+                    ->name('getTableData');
+                Route::get('exportExcel', 'PersonTableController@excel')
+                    ->name('exportExcel');
 
-            Route::get('initTable', 'PersonTableController@init')
-                ->name('initTable');
-            Route::get('getTableData', 'PersonTableController@data')
-                ->name('getTableData');
-            Route::get('exportExcel', 'PersonTableController@excel')
-                ->name('exportExcel');
-        });
+                Route::get('options', 'PersonSelectController@options')
+                    ->name('options');
+            });
 
         Route::resource('people', 'PersonController', ['except' => ['index', 'show']]);
     });
