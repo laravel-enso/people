f8d343fffa21afbfb09376056eddc9f3ddb1aaef
y0net <ionutpirvulescu1@gmail.com>
2018-11-05 17:06:39 +0200

    adds phpunit tests

---
+++ tests/features/PersonTest.php
@@ -0,0 +1,89 @@
+<?php
+
+use Faker\Factory;
+use Tests\TestCase;
+use LaravelEnso\Core\app\Models\User;
+use LaravelEnso\People\app\Models\Person;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use LaravelEnso\FormBuilder\app\TestTraits\EditForm;
+use LaravelEnso\FormBuilder\app\TestTraits\DestroyForm;
+use LaravelEnso\VueDatatable\app\Traits\Tests\Datatable;
+
+class PersonTest extends TestCase
+{
+    use Datatable, DestroyForm, EditForm, RefreshDatabase;
+
+    private $permissionGroup = 'administration.people';
+    private $testModel;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // $this->withoutExceptionHandling();
+
+        $this->seed()
+            ->actingAs(User::first());
+
+        $this->testModel = factory(Person::class)
+            ->make();
+    }
+
+    /** @test */
+    public function can_view_create_form()
+    {
+        $this->get(route($this->permissionGroup.'.create', [$this->testModel->id], false))
+            ->assertStatus(200)
+            ->assertJsonStructure(['form']);
+    }
+
+    /** @test */
+    public function can_store_person()
+    {
+        $response = $this->post(
+            route('administration.people.store', [], false),
+            $this->testModel->toArray()
+        );
+
+        $person = Person::whereEmail($this->testModel->email)
+            ->first();
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message'])
+            ->assertJsonFragment([
+                'redirect' => 'administration.people.edit',
+                'param' => ['person' => $person->id],
+            ]);
+    }
+
+    /** @test */
+    public function can_update_person()
+    {
+        $this->testModel->save();
+
+        $this->testModel->name = 'updated';
+
+        $this->patch(
+            route('administration.people.update', $this->testModel->id, false),
+            $this->testModel->toArray()
+        )->assertStatus(200)
+        ->assertJsonStructure(['message']);
+
+        $this->assertEquals('updated', $this->testModel->fresh()->name);
+    }
+
+    /** @test */
+    public function get_option_list()
+    {
+        $this->testModel->save();
+
+        $this->get(route('administration.people.options', [
+            'query' => $this->testModel->name,
+            'limit' => 10,
+        ], false))
+        ->assertStatus(200)
+        ->assertJsonFragment([
+            'name' => $this->testModel->name,
+        ]);
+    }
+}
