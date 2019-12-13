19389c7806b1cdacca91e04a3a16081babffad66
Mihai <mihai.ocneanu@gmail.com>
2019-12-13 17:57:21 +0200

    split routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,15 +5,5 @@ Route::namespace('LaravelEnso\People\app\Http\Controllers')
     ->prefix('api/administration/people')
     ->as('administration.people.')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{person}/edit', 'Edit')->name('edit');
-        Route::patch('{person}', 'Update')->name('update');
-        Route::delete('{person}', 'Destroy')->name('destroy');
-
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
-
-        Route::get('options', 'Options')->name('options');
+        require 'app/people.php';
     });
