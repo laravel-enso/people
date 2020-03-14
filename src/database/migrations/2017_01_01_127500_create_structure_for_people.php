5b2bce7c53272f079f1eddcb39c25abeb7e3ff13
mishu <mishu@xtelecom.ro>
2020-03-14 18:34:01 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_127500_create_structure_for_people.php
+++ src/database/migrations/2017_01_01_127500_create_structure_for_people.php
@@ -1,21 +1,20 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForPeople extends Migration
 {
     protected $permissions = [
-        ['name' => 'administration.people.initTable', 'description' => 'Init table for people', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.people.tableData', 'description' => 'Get table data for people', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.people.exportExcel', 'description' => 'Export excel for people', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.people.options', 'description' => 'Get options for select', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.people.create', 'description' => 'Create person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.people.edit', 'description' => 'Edit existing person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.people.index', 'description' => 'Show people index', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.people.store', 'description' => 'Store newly created person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.people.update', 'description' => 'Update edited person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.people.destroy', 'description' => 'Delete person', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'administration.people.initTable', 'description' => 'Init table for people', 'is_default' => false],
+        ['name' => 'administration.people.tableData', 'description' => 'Get table data for people', 'is_default' => false],
+        ['name' => 'administration.people.exportExcel', 'description' => 'Export excel for people', 'is_default' => false],
+        ['name' => 'administration.people.options', 'description' => 'Get options for select', 'is_default' => false],
+        ['name' => 'administration.people.create', 'description' => 'Create person', 'is_default' => false],
+        ['name' => 'administration.people.edit', 'description' => 'Edit existing person', 'is_default' => false],
+        ['name' => 'administration.people.index', 'description' => 'Show people index', 'is_default' => false],
+        ['name' => 'administration.people.store', 'description' => 'Store newly created person', 'is_default' => false],
+        ['name' => 'administration.people.update', 'description' => 'Update edited person', 'is_default' => false],
+        ['name' => 'administration.people.destroy', 'description' => 'Delete person', 'is_default' => false],
     ];
 
     protected $menu = [
