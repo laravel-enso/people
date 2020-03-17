569c58ec7cc40f86824de81af64ccbb10f49ad41
mishu <mishu@xtelecom.ro>
2020-03-17 10:49:28 +0200

    adds person resource

---
+++ src/app/Http/Resources/Person.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LaravelEnso\People\App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Config;
+use LaravelEnso\People\App\Enums\Titles;
+
+class Person extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'appellative' => $this->appellative,
+            'birthday' => $this->birthday->format(Config::get('config.enso.dateFormat')),
+            'title' => Titles::get($this->title),
+            'phone' => $this->phone,
+        ];
+    }
+}
