b113184da9ed120d3e0a15c05d2849ba7a315c53
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:30:46 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\People\Http\Controllers\Create;
+use LaravelEnso\People\Http\Controllers\Destroy;
+use LaravelEnso\People\Http\Controllers\Edit;
+use LaravelEnso\People\Http\Controllers\ExportExcel;
+use LaravelEnso\People\Http\Controllers\InitTable;
+use LaravelEnso\People\Http\Controllers\Options;
+use LaravelEnso\People\Http\Controllers\Store;
+use LaravelEnso\People\Http\Controllers\TableData;
+use LaravelEnso\People\Http\Controllers\Update;
 
-Route::namespace('LaravelEnso\People\Http\Controllers')
-    ->middleware(['api', 'auth', 'core'])
+Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/administration/people')
     ->as('administration.people.')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{person}/edit', 'Edit')->name('edit');
-        Route::patch('{person}', 'Update')->name('update');
-        Route::delete('{person}', 'Destroy')->name('destroy');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{person}/edit', Edit::class)->name('edit');
+        Route::patch('{person}', Update::class)->name('update');
+        Route::delete('{person}', Destroy::class)->name('destroy');
 
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
+        Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
 
-        Route::get('options', 'Options')->name('options');
+        Route::get('options', Options::class)->name('options');
     });
