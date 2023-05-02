5a75f5f3d8af85fab65cb22fe51dcc19d4600f06
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2023-05-02 16:46:01 +0100

    remove validator

--- src/Imports/Importers/Person.php
+++ src/Imports/Importers/Person.php
@@ -5,12 +5,12 @@ namespace LaravelEnso\People\Imports\Importers;
 use LaravelEnso\DataImport\Contracts\Importable;
 use LaravelEnso\DataImport\Models\Import;
 use LaravelEnso\Helpers\Services\Obj;
-use LaravelEnso\People\Models\Person;
+use LaravelEnso\People\Models\Person as Model;
 
-class People implements Importable
+class Person implements Importable
 {
     public function run(Obj $row, Import $import)
     {
-        Person::factory()->create($row->toArray());
+        Model::factory()->create($row->toArray());
     }
 }
