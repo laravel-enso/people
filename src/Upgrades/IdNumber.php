4ad96331c5badfbfc94f5f4962f4e7fbacdeaa94
mishu <mishu@xtelecom.ro>
2023-05-31 07:54:08 +0300

    fixes upgrade order

--- src/Upgrades/IdNumber.php
+++ src/Upgrades/IdNumber.php
@@ -5,9 +5,15 @@ namespace LaravelEnso\People\Upgrades;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
+use LaravelEnso\Upgrade\Contracts\Prioritization;
 
-class IdNumber implements MigratesTable
+class IdNumber implements MigratesTable, Prioritization
 {
+    public function priority(): int
+    {
+        return 110;
+    }
+
     public function isMigrated(): bool
     {
         return Schema::getColumnType('people', 'id_number') === 'string';
