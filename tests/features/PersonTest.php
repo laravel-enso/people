49a82ac77f4ac202e006b617f7d75410f09846ad
mishu <mishu@xtelecom.ro>
2026-04-21 18:26:56 +0300

    Apply local changes

--- tests/features/PersonTest.php
+++ tests/features/PersonTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use LaravelEnso\Companies\Models\Company;
 use LaravelEnso\Forms\TestTraits\CreateForm;
 use LaravelEnso\Forms\TestTraits\DestroyForm;
 use LaravelEnso\Forms\TestTraits\EditForm;
@@ -87,5 +88,56 @@ class PersonTest extends TestCase
         ], false))
             ->assertStatus(200)
             ->assertJsonFragment(['name' => $this->testModel->name]);
+    }
+
+    #[Test]
+    public function validates_that_main_company_belongs_to_selected_companies()
+    {
+        $company = Company::factory()->test()->create();
+        $otherCompany = Company::factory()->test()->create();
+
+        $this->post(route('administration.people.store', [], false), [
+            ...$this->testModel->toArray(),
+            'companies' => [$company->id],
+            'company' => $otherCompany->id,
+        ])->assertStatus(302)
+            ->assertSessionHasErrors(['company']);
+    }
+
+    #[Test]
+    public function syncs_companies_and_marks_main_company_on_store()
+    {
+        $firstCompany = Company::factory()->test()->create();
+        $secondCompany = Company::factory()->test()->create();
+
+        $this->post(route('administration.people.store', [], false), [
+            ...$this->testModel->toArray(),
+            'companies' => [$firstCompany->id, $secondCompany->id],
+            'company' => $secondCompany->id,
+        ])->assertStatus(200);
+
+        $person = Person::whereEmail($this->testModel->email)->firstOrFail();
+
+        $this->assertCount(2, $person->companies);
+        $this->assertSame($secondCompany->id, $person->company()->id);
+        $this->assertTrue((bool) $person->companies->firstWhere('id', $secondCompany->id)->pivot->is_main);
+        $this->assertFalse((bool) $person->companies->firstWhere('id', $firstCompany->id)->pivot->is_main);
+    }
+
+    #[Test]
+    public function forbids_changing_email_for_person_that_has_a_user()
+    {
+        $person = Person::factory()->test()->create();
+
+        User::factory()->create([
+            'person_id' => $person->id,
+            'email' => $person->email,
+        ]);
+
+        $this->patch(route('administration.people.update', $person->id, false), [
+            ...$person->toArray(),
+            'email' => 'changed@example.com',
+            'companies' => [],
+        ])->assertStatus(403);
     }
 }
